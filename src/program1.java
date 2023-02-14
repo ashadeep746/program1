9564dd8b405e5973b0a9fffc3f28424944f4bff4
ashadeep <ashadeep746@gmail.com>
2023-02-15 05:17:38 +0530

    Added: program1

---
+++ src/program1.java
@@ -0,0 +1,29 @@
+public  class program1 {
+    static int i;
+    static int j;
+   static int[] temp;
+    static int[][] arr = {{5, 6,}, {7, 8}};
+
+    //int[] largest number;
+    {
+        for (i=1; i<=arr.length; i++) {
+            for (j=1; j<=arr.length-1; j++) {
+
+            }
+        }
+        //swap
+        temp=arr[i];
+        arr[j]=arr[i];
+        arr[j]=temp;
+        //System.out.println(arr[i]);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(temp);
+    }
+}
+
+
+
+
